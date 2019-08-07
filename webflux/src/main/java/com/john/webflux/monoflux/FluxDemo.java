e9f6ed5b350f3e2a6fabef2e5c51f42c35188bf2
johnwall <1047994907@qq.com>
2019-08-07 20:20:49 +0800

    webflux学习: reactor原生实现了响应式流规范的框架，spring-webflux默认使用的响应式流库。flux和mono都会监听三种事件，1：有正常元素要处理的事件，2：序列结束事件，3：序列发生异常事件

--- webflux/src/main/java/com/john/webflux/monoflux/FluxDemo.java
+++ webflux/src/main/java/com/john/webflux/monoflux/FluxDemo.java
@@ -1,11 +1,56 @@
 package com.john.webflux.monoflux;
 
+import com.google.common.annotations.VisibleForTesting;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
 /**
  * @author zhangjuwa
  * @apiNote
  * @date 2019-08-07 00:17
  * @since jdk1.8
  */
+@Slf4j
 public class FluxDemo {
+
+    /**
+     * 构造flux的方法
+     */
+    @Test
+    public void constructure() {
+//        可以指定序列中包含的全部元素。创建出来的 Flux 序列在发布这些元素之后会自动结束。
+        Flux<Integer> just = Flux.just(1, 2);
+        log.info("just个数={}", just.count().block());
+        ArrayList<String> list = Lists.newArrayList("2", "b", "4");
+//        可以从一个数组、Iterable 对象或 Stream 对象中创建 Flux 对象。
+        Flux<String> stringFlux = Flux.fromIterable(list);
+        Flux<String> stringFlux1 = Flux.fromArray(new String[]{"a", "b"});
+        Flux<String> stringFlux2 = Flux.fromStream(list.stream());
+//        empty()：创建一个不包含任何元素，只发布结束消息的序列。
+        Flux<Object> empty = Flux.empty();
+//        error(Throwable error)：创建一个只包含错误消息的序列。
+        Flux<Object> flux = Flux.error(new IllegalAccessError("内部错误"));
+//        never()：创建一个不包含任何消息通知的序列。
+        Flux<Object> never = Flux.never();
+//        创建包含从 start 起始的 count 个数量的 Integer 对象的序列。
+        Flux<Integer> range = Flux.range(0, 10);
+
+    /*    interval(Duration period)和 interval(Duration delay, Duration period)：创建一个包含了从 0 开始递增的
+        Long 对象的序列。其中包含的元素按照指定的间隔来发布。除了间隔时间之外，还可以指定起始元素发布之前的延迟时间。*/
+        Flux<Long> interval = Flux.interval(Duration.ofMillis(10));
+    }
+
+    /**
+     * 上面的这些静态方法适合于简单的序列生成，当序列的生成需要复杂的逻辑时，则应该使用 generate() 或 create() 方法。
+     */
+    @Test
+    public void generate() {
+
+    }
 
 }
