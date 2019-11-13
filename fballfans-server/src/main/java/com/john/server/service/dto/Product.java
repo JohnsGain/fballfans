15107eddf6efdc329e78e816181af4fb363ddff8
zhangjw <1047994907@qq.com>
2019-11-13 18:11:20 +0800

    添加业务服务

--- fballfans-server/src/main/java/com/john/server/service/dto/Product.java
+++ fballfans-server/src/main/java/com/john/server/service/dto/Product.java
@@ -3,6 +3,8 @@ package com.john.server.service.dto;
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author zhangjuwa
  * @apiNote
@@ -10,18 +12,20 @@ import lombok.Data;
  * @since jdk1.8
  */
 @Data
+
 public class Product {
 
-    @Excel(name = "一级_一级类目编号")
+    @Excel(name = "一级类目编号")
     private String firstCode;
 
-    @Excel(name = "一级_一级类目")
+    @Excel(name = "一级类目")
     private String firstName;
 
-    @Excel(name = "二级_二级类目编号")
+    @Excel(name = "二级类目编号")
     private String secondCode;
 
-    @Excel(name = "二级_二级类目")
+    @Excel(name = "二级类目")
+    @NotNull(message = "不能为空")
     private String secondName;
 
 
--- fballfans-server/src/main/java/com/john/server/test/ImoritTest.java
+++ fballfans-server/src/main/java/com/john/server/test/ImoritTest.java
@@ -2,6 +2,7 @@ package com.john.server.test;
 
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
+import cn.afterturn.easypoi.excel.entity.result.ExcelImportResult;
 import cn.afterturn.easypoi.util.PoiPublicUtil;
 import com.john.server.service.dto.Product;
 import org.junit.Test;
@@ -20,14 +21,17 @@ public class ImoritTest {
     @Test
     public void imports() {
         ImportParams params = new ImportParams();
-        params.setTitleRows(1);
+        params.setTitleRows(2);
         params.setHeadRows(1);
 //        ExcelImportUtil.importExcel(
 //                new File(PoiPublicUtil.getWebRootPath("import/ExcelExportMsgClient.xlsx")),
 //                MsgClient.class, params);
 
-        List<Product> excel = ExcelImportUtil.importExcel(new File("/Users/ligeit/Desktop/工作簿1.xlsx"), Product.class, params);
-        System.out.println(excel.size());
+//        List<Product> excel = ExcelImportUtil.importExcel(new File("/Users/ligeit/Desktop/工作簿1.xlsx"), Product.class, params);
+//        System.out.println(excel.size());
+        ExcelImportResult<Product> excel = ExcelImportUtil.importExcelMore(new File("/Users/ligeit/Desktop/工作簿1.xlsx"), Product.class, params);
+        List<Product> failList = excel.getFailList();
+//        System.out.println(failList.stream().map(item->item.get));
     }
 
 }
