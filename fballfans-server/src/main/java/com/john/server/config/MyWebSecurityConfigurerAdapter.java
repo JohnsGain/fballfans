59825001cdd1a197eaeb9affd2e93663707b9ca5
zhangjw <1047994907@qq.com>
2019-11-26 14:54:11 +0800

    练习使用git flow release ,在预发布分支可以修复代码，添加文档，不再加入新开发功能。完成之后，合并到master。

--- fballfans-server/src/main/java/com/john/server/config/MyWebSecurityConfigurerAdapter.java
+++ fballfans-server/src/main/java/com/john/server/config/MyWebSecurityConfigurerAdapter.java
@@ -14,7 +14,7 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/**").authenticated()
                 .and()
                 .cors()
                 .and()
