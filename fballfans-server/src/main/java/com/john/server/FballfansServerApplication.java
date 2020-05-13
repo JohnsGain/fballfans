6c3d685bee5bb22805bf84ad98680846f6f6b53a
zhangjuwa <1047994907@qq.com>
2020-05-13 11:15:21 +0800

    comm3

--- fballfans-server/src/main/java/com/john/server/FballfansServerApplication.java
+++ fballfans-server/src/main/java/com/john/server/FballfansServerApplication.java
@@ -21,7 +21,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 public class FballfansServerApplication {
 
     public static void main(String[] args) {
-        //xx XX
+        //xx XX XXX
         SpringApplication.run(FballfansServerApplication.class, args);
     }
 
